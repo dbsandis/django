b5f37ee21908f3293de0ad2b49eaf3fea1e4a47f
DARYL SANDERS <dbsandis@gmail.com>
2023-12-25 21:27:14 -0500

    commit through lesson 5

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
@@ -19,3 +18,7 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- polls/views.py
+++ polls/views.py
@@ -2,17 +2,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
-
-from .models import Choice, Question
-
-# Create your views here.
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
 from django.views import generic
 
 from .models import Choice, Question
-
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
